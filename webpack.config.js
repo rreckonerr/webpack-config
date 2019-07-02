56c2e25ff84d08b6043214ca79b357c756f02a92
rreckonerr <rreckonerr@gmail.com>
2019-07-02 14:13:59 +0300

    remove dev comments and packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,18 @@ var path = require('path');
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// ! Dev only
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: __dirname,
 
   entry: {
-    //! Dev only
-    main: './modules/one',
-    lpCore: './modules/two',
-    lpEmc1: './modules/three'
-    // main: './modules/cms/promo_pages/static/js/promo-page',
-    // lpCore: './modules/cms/landing_pages/static/scripts/core',
-    // lpEmc1: './modules/cms/landing_pages/static/scripts/emc1'
+    main: './modules/cms/promo_pages/static/js/promo-page',
+    lpCore: './modules/cms/landing_pages/static/scripts/core',
+    lpEmc1: './modules/cms/landing_pages/static/scripts/emc1'
   },
 
   output: {
-    //! Dev only
-    path: path.resolve('./dist'),
-    // path: path.resolve('./staticfiles/assets/bundles/'),
+    path: path.resolve('./staticfiles/assets/bundles/'),
     filename: '[name]-[hash].js'
   },
 
@@ -31,8 +23,6 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery'
     }),
-    //! Dev only
-    new HtmlWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css'
@@ -48,11 +38,8 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          //* Provides css chunks
           MiniCssExtractPlugin.loader,
           'css-loader',
-
-          //* Provides autoprefixer
           {
             loader: 'postcss-loader',
             options: {
